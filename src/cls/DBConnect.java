2261d9f0945dae1a645aadb0cccace6e451ea8b0
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2023-08-05 10:04:55 +0200

    database connection class

---
+++ src/cls/DBConnect.java
@@ -0,0 +1,32 @@
+package cls;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author esad
+ */
+public class DBConnect {
+  private static String connUrl   = "jdbc:mysql://localhost/dbRentACar";
+  private static String connUser = "root";
+  private static String connPassword    = "";
+  
+  public static Connection uspostaviKonekciju(){
+      try{
+          Class.forName("com.mysql.jdbc.Driver");
+          Connection kon = DriverManager.getConnection(connUrl, connUser, connPassword);
+          //JOptionPane.showMessageDialog(null, "Veza sa bazom podataka je uspostavljena!");
+          return kon;
+      }catch (Exception e){
+          JOptionPane.showMessageDialog(null, "Greška u konekciji sa bazom podataka: "+e.getMessage());
+          return null;
+      }
+  }
+}
